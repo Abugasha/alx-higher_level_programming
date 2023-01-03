472c91a2f84755680001cb5a2ff568afc66a0faa
Shihab <shihabunimap@gmail.com>
2023-01-03 03:56:09 +0200

    fixx

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,13 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdlib.h>
+
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for Holberton project
+*/
 typedef struct listint_s
 {
 	int n;

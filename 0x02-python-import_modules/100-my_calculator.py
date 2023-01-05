dfa2e635a76f27fe91bc23109ecece90a9f141c9
Abugasha <shihabunimap@gmail.com>
2023-01-05 19:38:26 +0000

    fix advance

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+    if (len(sys.argv) != 4):
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        exit(1)
     from calculator_1 import add, sub, mul, div
-    args = len(sys.argv)
-    if(args != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    operator = sys.argv[2]
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if(operator in "+-*/"):
-        if operator is "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator is "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator is "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        else:
-            print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
+    symbol = sys.argv[2]
+    operators = {
+        '+': add(a, b),
+        '-': sub(a, b),
+        '*': mul(a, b),
+        '/': div(a, b)
+    }
+    try:
+        print("{:d} {} {:d} = {:d}".format(a, symbol, b, operators[symbol]))
+    except KeyError:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)

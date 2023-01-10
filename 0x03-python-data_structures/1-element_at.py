87be56f343946f5339057c84cd7d2cecfa7c97a4
Shihab <shihabunimap@gmail.com>
2023-01-10 03:28:26 +0200

    Delete 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    """Gets element in list, if not found returns None"""
-    if (idx + 1 > len(my_list) or idx < 0):
-        return (None)
-    else:
-        return(my_list[idx])
-

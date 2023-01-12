b0d7deea651755d0f73939725dd0ee883239c82d
Abugasha <shihabunimap@gmail.com>
2023-01-12 00:06:51 +0000

    error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for i in range(0, len(matrix)):
-        new_matrix.append(list(map(power_2, matrix[i])))
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
     return (new_matrix)
-
-
-def power_2(x):
-    return (x ** 2)
 

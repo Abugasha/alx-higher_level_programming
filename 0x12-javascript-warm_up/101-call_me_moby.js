69965fc6721ac1475aeeb0126486d060e48968d2
Shihab <shihabunimap@gmail.com>
2023-05-01 11:54:12 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-  let i;
-  for (i = 0; i < x; i++) {
-    theFunction();
-  }
-};
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-if (!isNaN(process.argv[2])) {
-  console.log('My number: ' + parseInt(process.argv[2]));
-} else {
-  console.log('Not a number');
-}
